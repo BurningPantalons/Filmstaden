b6bae842f755a6096cca4c6fc069c4f4aaf0d49a
nicholas <nicken333@gmail.com>
2021-02-01 13:14:17 +0100

    Merged with added readSaongJson function

--- main.js
+++ main.js
@@ -6,17 +6,16 @@ async function readMovieJson() {
   showDocumentAsHtml(jsonMovies);
 }
 
-<<<<<<< HEAD
-/* The "for...of" statement creates a loop iterating over iterable objects (document)
-   $document creates a div for each object.*/
-=======
 async function readSalongJson() {
   let jsonSalong = await $.getJSON("/JSON filer/salonger.json");
   console.log(jsonSalong);
   showSalongerAsHtml(jsonSalong)
 }
 
->>>>>>> 3118669511fd738555c36b2f5f4d7f90ae8934f9
+
+/* The "for...of" statement creates a loop iterating over iterable objects (document)
+   $document creates a div for each object.*/
+
 function showDocumentAsHtml(collection) {
 
   for (let document of collection) {
