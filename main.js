3118669511fd738555c36b2f5f4d7f90ae8934f9
malmz90 <70096644+malmz90@users.noreply.github.com>
2021-02-01 13:03:55 +0100

    Läs in json, skapa salonglista
    la till main i bodyn. med div klasser left och rightside bara test för att se hur de ser ut. Enkel css för klasser. liten ändring i function. showDocumentAsHtml
    på append.

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ async function readMovieJson() {
   showDocumentAsHtml(jsonMovie)
 }
 
+async function readSalongJson() {
+  let jsonSalong = await $.getJSON("/JSON filer/salonger.json");
+  console.log(jsonSalong);
+  showSalongerAsHtml(jsonSalong)
+}
+
 function showDocumentAsHtml(collection) {
 
   for (let document of collection) {
@@ -15,10 +21,29 @@ function showDocumentAsHtml(collection) {
       $document.append('<div><span>' + key + ': <span>' + value + '</div>');
     }
 
-    $('body').append($document);
-    $('body').append("<br>");
+    $('.rightside').append($document);
+    $('.rightside').append("<br>");
 
   }
 }
 
+function showSalongerAsHtml(collection) {
+
+  for (let document of collection) {
+    let $document = $('<div class="document"></div>');
+
+    for (let key in document) {
+      let value = document[key];
+      $document.append('<div><span>' + key + ': <span>' + value + '</div>');
+    }
+
+    $('.leftside').append($document);
+    $('.leftside').append("<br>");
+
+  }
+}
+
+
+
+readSalongJson();
 readMovieJson();
