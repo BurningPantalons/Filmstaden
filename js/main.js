bbfc1a72230ffdbef1e7a5185247c1259446994b
malmz90 <70096644+malmz90@users.noreply.github.com>
2021-02-04 17:16:48 +0100

    Flytta function och la ny poster.
    Updaterade filmer.json med rätt poster på filmobjekt. flyttade functtion showMovieAsHtml till filmer.json

--- js/main.js
+++ js/main.js
@@ -21,29 +21,4 @@ async function readVisiningJson() {
 }
 
 
-/* The "for...of" statement creates a loop iterating over iterable objects (document)
-   $document creates a div for each object.*/
 
-function showMovieAsHtml(collection, className) {
-
-  for (let document of collection) {
-    let $document = $(`<div class="${className}"></div>`);
-
-    for (let key in document) {
-      let value = document[key];
-      
-    if (key == "Title") {
-      $document.append(`<h1 class="title"> ${value} </h1>`);
-    }
-    if (key == "Year") {
-      $document.append(`<h2> Årtal ${value} </h2>`);
-      }
-      if (key == "Poster") { 
-        $document.append(`<img src=${value}>`)
-      }
-
-    $('.rightside').append($document);
-
-  }
-}
-}
