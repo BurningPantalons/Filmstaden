d0b6c11067a30f39f9daa37ab8fc62811f8a2be1
n1cken <nicken333@gmail.com>
2021-02-08 22:43:55 +0100

    Started styling salong

--- js/salong.js
+++ js/salong.js
@@ -5,20 +5,27 @@ function showSalongerAsHtml(collection) {
 
     let $salong = $('<div class="salong"></div>');
     
+    
    //$salong.append('<form'>  ); 
 
     for (let key in salong) {
       let value = salong[key];
-      $salong.append('<div><span>' + key + ': <span>' + value + '</div>');
+
+      if (key == "name") {
+      $salong.append('<div class="salongName"> <span>' + value + '</div>');
+      $salong.append('<div class="filmduk">');
+
+
+      }
 
       if (key == "seatsPerRow") {
         let numOfSeats = value;
 
         for (let row = 0; row < numOfSeats[row]; row++) {
-          $salong.append('<div class="rowhead">' + "Row " + row + '</div>');
+          $salong.append('<div class="rowhead">' + "Rad " + row + '</div>');
           let $row = $('<div class="row"></div>');
           for (let seat = 0; seat < numOfSeats[row]; seat++) {
-            $row.append(`<input type="checkbox" value="${seat}">` +`<label>${seat}</label>`);
+            $row.append(`<input class="checkbox" type="checkbox" value="${seat}">` +`<label>${seat}</label>`);
             $salong.append($row);
           }
         }
