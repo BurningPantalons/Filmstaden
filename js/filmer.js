2c5c8f4af5d6aeae1d6430a28489baa2c3af77be
n1cken <nicken333@gmail.com>
2021-02-08 22:05:43 +0100

    Styled filmer.html/js

--- js/filmer.js
+++ js/filmer.js
@@ -15,18 +15,18 @@ function showMovieAsHtml(collection, className) {
   for (let document of collection) {
    
    let $document =  $(`<div class="${className}">
-  <img src=${document.Poster} />
+  <img class="poster" src=${document.Poster} />
 
   <div class="MovieText">
-   <h1 class="title">${document.Title}</h1>
+   <h1 class="Title">${document.Title}</h1>
    <div>
    <p class="Released"> Premiär: ${document.Released} </p> 
    <p class="Synopsis"> Handling: ${document.Plot} </p> 
    <p class="Genre"> Genre: ${document.Genre} </p> 
    </div>
    <div class="ButtonContainer">
-    <button onclick="location.href='/html/detaljsida.html?${document.MovieId}'" id=${document.MovieId} class="movieButton" type="button">Läs Mer!</button>
-    <button onclick="location.href='/html/platsbokning.html?${document.MovieId}'" class="movieButton" type="button">Boka Biljett!</button>
+    <button onclick="location.href='/html/detaljsida.html?${document.MovieId}'" id=${document.MovieId} class="movieButton" type="button">Läs Mer</button>
+    <button onclick="location.href='/html/platsbokning.html?${document.MovieId}'" class="movieButton" type="button">Boka Biljett</button>
   </div>
 
   </div>
