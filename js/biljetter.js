1a4517f245c5d3b7b124136b16657e801b753869
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2021-02-09 18:36:55 +0100

    changed name of createCalendar()

--- js/biljetter.js
+++ js/biljetter.js
@@ -1,7 +1,7 @@
 let filmval = $(`
   <div class="filmval">
     <label for="movies">Välj film:</label>
-    <select name="movies" id="movies" onchange="createCalendar(value);"></select>
+    <select name="movies" id="movies" onchange="createCalendarDays(); pickTime(value)"></select>
   </div>`);
   
 $('.movieChoice').append(filmval);
@@ -10,14 +10,14 @@ pickMovie();
 
 async function pickMovie(){
   let jsonMovies = await $.getJSON("/json/filmer.json");
-  let i = 0;
+  let i = 1;
   for (let movies of jsonMovies) {
-    let $option = $(`<option value="${i}">${movies.Title}</option>`)
+    let $option = $(`<option value="${i}">${movies.Title}</option>`);
     i++;
     $('#movies').append($option);
   }  
 };  
 
 function pickTime(value) {
-
+  console.log(value);
 }
