48d6557142fd016a8dd9f6de0fcd05cd9a656e73
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2021-02-09 12:29:44 +0100

    changed the loop iterations to start from 1

--- js/salong.js
+++ js/salong.js
@@ -21,10 +21,10 @@ function showSalongerAsHtml(collection) {
       if (key == "seatsPerRow") {
         let numOfSeats = value;
 
-        for (let row = 0; row < numOfSeats[row]; row++) {
+        for (let row = 1; row < numOfSeats[row]; row++) {
           $salong.append('<div class="rowhead">' + "Rad " + row + '</div>');
           let $row = $('<div class="row"></div>');
-          for (let seat = 0; seat < numOfSeats[row]; seat++) {
+          for (let seat = 1; seat < numOfSeats[row]; seat++) {
             $row.append(`<input class="checkbox" type="checkbox" value="${seat}">` +`<label>${seat}</label>`);
             $salong.append($row);
           }
